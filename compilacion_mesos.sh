11ae5b4c71631cda40e135bcb349a54e43ce5c0e
Marcos Bjorkelund <marcosbjorkelund@gmail.com>
2016-12-29 23:15:49 +0100

    Separar scripts de instalacion y compilacion de Mesos y Kubernetes, despues de mejoras

---
+++ compilacion_mesos.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# Este script se encarga de instalar Docker en una maquina CentOS 7
+
+echo "Ejecutando $0 a las `date +%H:%M:%S`"
+
+# En caso de error, no seguir con la ejecucion
+set -e
+
+# Comprobamos usuario
+if [ "$UID" != "0" ]; then
+    echo "Es necesario ejecutar este script como el usuario 'root'."
+    exit 1
+fi
+
+# Actualizar paquetes con yum
+# yum -y update
+
+# Instalar dependencias
+yum -y install git tar wget epel-release
+
+# Para instalar maven
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+
+# Para instalar subversion devel (>1.8)
+cat > /etc/yum.repos.d/wandisco-svn.repo <<EOF
+[WANdiscoSVN]
+name=WANdisco SVN Repo 1.9
+enabled=1
+baseurl=http://opensource.wandisco.com/centos/7/svn-1.9/RPMS/\$basearch/
+gpgcheck=1
+gpgkey=http://opensource.wandisco.com/RPM-GPG-KEY-WANdisco
+EOF
+
+# Actualizar systemd
+SYSTEMD_VERSION=$(systemctl --version | grep systemd | awk '{ print $2 }')
+if [ "$SYSTEMD_VERSION" -lt 218 ]; then
+    echo "Version de systemd menor a 218: $SYSTEMD_VERSION"
+    exit 1
+fi
+echo "Version de systemd valida: $SYSTEMD_VERSION"
+
+# Instalar development tools
+yum groupinstall -y "Development Tools"
+
+# Instalar Maven y otras dependencias de Mesos
+for LIB in apache-maven python-devel java-1.8.0-openjdk-devel zlib-devel libcurl-devel openssl-devel cyrus-sasl-devel cyrus-sasl-md5 apr-devel subversion-devel apr-util-devel apr-1.4.8-3.el7.i686 apr-devel curl-devel cyrus-sasl subversion-devel; do
+    yum install -y $LIB
+done
+
+# Arreglar compilacion de mesos
+cd /usr/lib64
+ln -s libsasl2.so.3 libsasl2.so.2
+
+# Compilar mesos
+if [ ! -d /opt/mesos ]; then
+    mkdir /opt/mesos
+    cd /tmp
+    wget http://www.apache.org/dist/mesos/1.1.0/mesos-1.1.0.tar.gz
+    tar xzf mesos-1.1.0.tar.gz -C /opt/mesos
+fi
+
+# Compilar e instalar Mesos
+cd /opt/mesos/mesos-1.1.0
+./bootstrap
+./configure
+make
+
+#export JAVA_HOME=/usr/lib/jvm/java-1.8.0/bin
+#export PATH=$PATH:$JAVA_HOME/bin
+
+echo "Ejecucion de $0 finalizada a las `date +%H:%M:%S`"
+
--- instalacion_kubernetes.sh
+++ instalacion_kubernetes.sh
@@ -17,60 +17,7 @@ if [ "$UID" != "0" ]; then
     exit 1
 fi
 
-for HOMEDIR in /home/dit /root; do
-    if [ -f "$HOMEDIR/.bashrc" ]; then
-        if ! grep -q GOPATH=$GOPATH "$HOMEDIR/.bashrc"; then
-            echo "export GOPATH=$GOPATH
-export PATH=\$PATH:$GOROOT/bin:\$GOPATH/bin" >> "$HOMEDIR/.bashrc"
-        fi
-    fi
-done
-
-# Instalar Mercurial
-yum -y install mercurial
-
-# Instalar Go
-if [ ! -d /usr/local/go ]; then
-    cd /tmp
-    wget https://storage.googleapis.com/golang/go1.7.3.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.7.3.linux-amd64.tar.gz
-    rm go1.7.3.linux-amd64.tar.gz
-fi
-
-# Nos aseguramos que Go esta presente
-which go
-
-# Instalar godep
-go get -u github.com/tools/godep
-go get -u github.com/jteeuwen/go-bindata/go-bindata
-
-# Nos aseguramos que godep este presente
-godep version
-
-# Desactivamos el firewall para evitar problemas con reglas de iptables de Docker
-systemctl stop firewalld
-systemctl disable firewalld
-
-# Instalamos ntp
-yum -y install ntp
-systemctl start ntpd
-systemctl enable ntpd
-
 # Instalamos etcd
-yum -y install etcd
-
-# Obtener el codigo fuente de Kubernetes
-if [ ! -d "$GOPATH/src/k8s.io" ]; then
-    mkdir -p $GOPATH/src/k8s.io
-    cd $GOPATH/src/k8s.io
-    git clone https://github.com/kubernetes/kubernetes.git
-fi
-
-# Construimos Kubernetes desde el codigo fuente
-# Esto se debe a que ... TODO
-cd $GOPATH/src/k8s.io/kubernetes
-make clean
-make
-make install
+yum -y install etcd kubernetes
 
 echo "Ejecucion de $0 finalizada a las `date +%H:%M:%S`"
